42271d9e5424f8ab77d316625275dbff460585c2
Erick Kramer <erick.romero.kramer@fkie.fraunhofer.de>
2019-07-03 18:16:48 +0200

    Created file to generate a simple scenario for the arm

---
+++ scenarii/scenario_simple_arm.cpp
@@ -0,0 +1,13 @@
+//author: Erick Kramer
+//email: erickkramer@gmail.com
+
+#include <iostream>
+#include <cmath>
+
+//#include <boost/foreach.hpp>
+#include <boost/multi_array.hpp>
+
+int main(){
+
+    
+}
