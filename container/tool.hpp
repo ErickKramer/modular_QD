ec87160650e2b473d32d6b63151ef968fe9fa013
Erick Kramer <erick.romero.kramer@fkie.fraunhofer.de>
2019-07-08 15:07:18 +0200

    Organized the structure and added comments where possible

--- container/tool.hpp
+++ container/tool.hpp
@@ -2,40 +2,37 @@
 #define QD_CONTAINER_TOOL_HPP
 
 
-
-static double sign(double d)
-{
-  if(d < 0)
-    return -1;
-  return 1;
+// Returns the sign of d
+static double sign(double d){
+    if(d < 0)
+        return -1;
+    return 1;
 }
 
+// TODO: What is this function used for?
 template<typename V1, typename V2>
-double _dist(const V1& v1, const V2& v2)
-{
-  assert(v1.size() == v2.size());
-  typename V1::const_iterator it1 = v1.begin(), it2 = v2.begin();
-  double res = 0.0f;
-  while (it1 != v1.end())
-    {
-      double v = (float)*it1 - (float)*it2;
-      res += v * v;
-      ++it1;
-      ++it2;
+double _dist(const V1& v1, const V2& v2){
+    assert(v1.size() == v2.size());
+    typename V1::const_iterator it1 = v1.begin(), it2 = v2.begin();
+    double res = 0.0f;
+    while (it1 != v1.end()){
+        double v = (float)*it1 - (float)*it2;
+        res += v * v;
+        ++it1;
+        ++it2;
     }
-  return sqrt( res);
+    return sqrt( res);
 }
 
+// What is this struct used for?
 template<typename T>
-struct _compare_dist_f
-{
-  _compare_dist_f(const T& v) : _v(v) {}
-  const T _v;
-  bool operator()(const T& v1, const T& v2) const
-  {
-    assert(v1.size() == _v.size());
-    assert(v2.size() == _v.size());
-    return _dist(v1, _v) < _dist(v2, _v);
-  }
+struct _compare_dist_f{
+    _compare_dist_f(const T& v) : _v(v) {}
+    const T _v;
+    bool operator()(const T& v1, const T& v2) const{
+        assert(v1.size() == _v.size());
+        assert(v2.size() == _v.size());
+        return _dist(v1, _v) < _dist(v2, _v);
+    }
 };
 #endif
