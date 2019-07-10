568214717146762f765138fca4e80e87f3ff4aea
Erick Kramer <erick.romero.kramer@fkie.fraunhofer.de>
2019-07-10 18:47:50 +0200

    New file definition for a custom robotic arm

---
+++ scenarii/custom_arm.hpp
@@ -0,0 +1,25 @@
+#ifndef ROBOT_CUSTOM_ARM_HPP // TODO
+#define ROBOT_CUSTOM_ARM_HPP
+
+#include <Eigen/Core>
+
+// Definition of the arm
+namespace robot{
+    class CustomArm{
+        public:
+            static Eigen::Vector3d forward_model(Eigen::VectorXd angles);
+            static double max_length(){
+                return l1 + l2 + l3 + l4 + l5 + l6 + l7;
+            }
+
+        protected:
+            // Based the file to a real arm
+            static constexpr double l1 = 0.5;
+            static constexpr double l2 = 0.5;
+            static constexpr double l3 = 0.5;
+            static constexpr double l4 = 0.5;
+            static constexpr double l5 = 0.5;
+            static constexpr double l6 = 0.5;
+            static constexpr double l7 = 0.5;
+    }
+}
